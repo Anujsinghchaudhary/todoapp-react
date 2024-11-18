7f8c6d9e289a215894faa0774b6010a8593916ca
AnujSingh07 <anujsinghchaudhary07@gmail.com>
2024-11-18 22:23:11 +0530

    new code

---
+++ src/components/Todo.jsx
@@ -0,0 +1,22 @@
+import react from 'react';
+import './todo.css';
+import { useState } from'react';
+
+export const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    return (
+    <section className="todoapp">
+        <header>
+            <h1>Todos List</h1>
+        </header>
+        <form>
+            <div>
+                <input type="text" placeholder="What needs to be done?" />
+                <button type="submit">Add Todo</button>
+            </div>
+        </form>
+       </section>
+    )
+}
+
+export default Todo;
