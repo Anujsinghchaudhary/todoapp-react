892c031ef251346b9cdedef1bb3836ddc7ac20a1
AnujSingh07 <anujsinghchaudhary07@gmail.com>
2024-12-21 11:25:18 +0530

    completed project

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -3,12 +3,12 @@ import tick from '../assets/tick.png';
 import not_tick from '../assets/not_tick.png';
 import delete_icon from '../assets/delete.png';
 
-export const Todoitems = ({ text, id, isComplete, deleteTodo, toggle }) => {
+export const Todoitems = ({ text, id, isCompleted, deleteTodo, toggle }) => {
   return (
     <div className='flex items-center my-3 gap-2'>
       <div onClick={() => {toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
-        <img src={isComplete ? tick : not_tick} alt="" className='w-7'  />
-        <p className={'text-slate-700 ml-4 text-[17px] decoration-slate-500${isComplete ? "line-through" : ""}'}>{text}</p>
+        <img src={isCompleted ? tick : not_tick} alt="" className='w-7'  />
+        <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isCompleted ? "line-through" : ""}`}>{text}</p>
       </div>
       <div>
         <img 
